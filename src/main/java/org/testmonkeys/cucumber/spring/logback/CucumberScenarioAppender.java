5e262db535bbd50894cf47adff379ff3f440cc9c
monofrei <onofrei.marin@gmail.com>
2022-01-20 11:12:12 +0100

    Added support for Pattern Encoder to appender (#99)
    * Added support for Pattern Encoder to appender
    
    * Fixed issue when the pattern is not specified. Now supports with and without a pattern
    
    Co-authored-by: Marin Onofrei <monofrei@mobiquityinc.com>

--- src/main/java/org/testmonkeys/cucumber/spring/logback/CucumberScenarioAppender.java
+++ src/main/java/org/testmonkeys/cucumber/spring/logback/CucumberScenarioAppender.java
@@ -1,5 +1,6 @@
 package org.testmonkeys.cucumber.spring.logback;
 
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import io.cucumber.java.Scenario;
@@ -9,14 +10,26 @@ import io.cucumber.java.Scenario;
  */
 public class CucumberScenarioAppender extends AppenderBase<ILoggingEvent> {
 
+    private PatternLayoutEncoder encoder;
+
     private Scenario getScenario() {
         return SpringContext.getBean(CucumberScenarioContext.class).getCurrentScenario();
     }
 
     @Override
     protected void append(ILoggingEvent iLoggingEvent) {
-        Scenario cukeScenario = getScenario();
-        if (cukeScenario != null)
-            cukeScenario.log(iLoggingEvent.getFormattedMessage());
+        String message = this.encode(iLoggingEvent);
+        Scenario scenario = getScenario();
+        if (scenario != null) scenario.log(message);
+    }
+
+    private String encode(ILoggingEvent loggingEvent) {
+        if (this.encoder == null) return loggingEvent.getFormattedMessage();
+        return new String(this.encoder.encode(loggingEvent));
+    }
+
+    public void setEncoder(PatternLayoutEncoder encoder) {
+        this.encoder = encoder;
+        this.encoder.start();
     }
 }
